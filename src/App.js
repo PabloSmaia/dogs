42892216cf635b59355cdc6c61ab00b6f70f618f
Pablo Maia <pablosmaia@gmail.com>
2022-11-11 23:39:03 -0300

    Criando as rotas e finalizando o Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
 
 function App() {
-  return <div className="App">app react </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
