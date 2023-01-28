484b18c5b88bc5afc46550636d4a7601bc3cae6b
Pablo Maia <pablosmaia@gmail.com>
2023-01-27 21:57:23 -0300

    finalizando header mobile e iniciando post photo

--- src/components/User/UserPhotoPost.js
+++ src/components/User/UserPhotoPost.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import useFetch from '../../Hooks/useFetch';
+import useForm from '../../Hooks/useForm';
+import Button from '../form/Button';
+import Input from '../form/Input';
+import styles from './UserPhotoPost.module.css';
 
 const UserPhotoPost = () => {
-  return <div>Postar foto</div>;
+  const peso = useForm();
+  const idade = useForm('number');
+  const nome = useForm('number');
+  const [img, setImage] = React.useState({});
+  const { data, error, loading, request } = useFetch();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleImgChange() {}
+
+  return (
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form action={handleSubmit}>
+        <Input label="None" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button> Enviar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default UserPhotoPost;
